7f0ada886363a4de8c3e9df44228e5610acb307f
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-11 22:04:50 +0900

    Update table.php

--- parts/table.php
+++ parts/table.php
@@ -45,7 +45,7 @@
      
      <td style="border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $meal; ?></td>
         
-     <td onclick="window.location='input/notices.php/<?php echo $id ?>/'"  style="border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $notices; ?>
+     <td onclick="window.location='input/notices.php/<?php echo $id ?>/'"  style="border:1px solid #ccc;background:#fff;padding:4px;"><?php if(isset($notices)){echo '〇'; } ?>
      </td>
      </tr>
 </tbody>
