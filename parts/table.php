b4897273ee750f895a51623613765e7ab65a2966
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-11 21:43:23 +0900

    Update table.php

--- parts/table.php
+++ parts/table.php
@@ -19,22 +19,18 @@
  ?>
 <tbody>
 <tr>
-    <!--お名前-->
     <td onclick="window.location='input/name.php/<?php echo $id ?>/'"  
      style="height:50px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $name; ?>
      </td>
            
-     <!--来所-->
     <td onclick="window.location='input/visit.php/<?php echo $id ?>/'"  
      style="border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $visit; ?>
      </td>
         
-     <!--退所-->
     <td onclick="window.location='input/exit.php/<?php echo $id ?>/'"  
      style="border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $exits; ?>
      </td>
         
-     <!--最高・最低血圧/脈拍-->
      <td onclick="window.location='input/blood.php/<?php echo $id ?>/'"  
      style="border:1px solid #ccc;background:#fff;padding:4px;">
      <?php if(isset($maxblood) || ($miniblood) ||( $pulse)){
@@ -42,25 +38,16 @@
      }
       ?>
      </td>
+    
+     <td style="border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $bath; ?></td>
+     
+     <td style="border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $meal; ?></td>
         
-      <!--入浴-->
-     <td  
-     style="border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $bath; ?>
-     </td>
-        
-        
-      <!--食事-->
-    <td   
-     style="border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $meal; ?>
-     </td>
-        
-      <!--特記事項-->
-    <td onclick="window.location='input/notices.php/<?php echo $id ?>/'"  
-     style="border:1px solid #ccc;background:#fff;padding:4px;"><?php echo mb_strimwidth($notices, 0, 7, '…', 'UTF-8'); ?>
+     <td onclick="window.location='input/notices.php/<?php echo $id ?>/'"  style="border:1px solid #ccc;background:#fff;padding:4px;"><?php echo mb_strimwidth($notices, 0, 7, '…', 'UTF-8'); ?>
      </td>
         
     </tr>
-<tbody>
+</tbody>
 <?php 
                 
       }
