5ac8a0fae944a3e641e112d4bdb393d19c53529f
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-23 19:19:24 +0900

    Update blood.php

--- input/blood.php
+++ input/blood.php
@@ -5,36 +5,48 @@
 <div style="width:500px;margin-left:250px;margin-top:50px;border:solid 1px gray;padding:40px;display:inline-block;">
 <form>
     <h4>血圧</h4>
-  <div class="form-group" style="margin-top:40px;">
-    <p>最高血圧</p>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" style="width:140px;">
-    <small id="emailHelp" class="form-text text-muted">※数字を入力してください</small></br>
+    <div style="margin-top:40px;">
+      
+        <p>最高血圧</p>
+        <input type="text" style="width:140px;" class="maxblood">
+        <small>※数字を入力してください</small></br>
 
-    <p style="margin-top:20px;">最低血圧</p>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" style="width:140px;">
-    <small id="emailHelp" class="form-text text-muted">※数字を入力してください</small></br>
+        <p style="margin-top:20px;">最低血圧</p>
+        <input type="text" style="width:140px;" class="miniblood">
+        <small>※数字を入力してください</small></br>
 
-    <p style="margin-top:20px;">脈拍</p>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" style="width:140px;margin-top:20px;">
-    <small id="emailHelp" class="form-text text-muted">※数字を入力してください</small></br>
-  </div>
-  <button type="submit" class="btn btn-primary" style="margin-left:340px;">送信</button>
+        <p style="margin-top:20px;">脈拍</p>
+        <input type="text" style="width:140px;margin-top:20px;" class="pulse">
+        <small>※数字を入力してください</small></br>
+
+    </div>
+
+    <button type="submit" class="btn btn-primary" style="margin-left:340px;">送信</button>
+
 </form>
+
 </div>
 
 <?php
-if(isset($_POST['form-control'])){
 
-try {
-    $stt = getDB()->prepare('UPDATE user SET maxblood = :maxblood where id = :id');
-    $stt->bindParam(':maxblood', $_POST['form-control']);
-    $stt->bindParam(':id', $id);
-    $stt->execute();
-    header('Location: https://animech2.herokuapp.com/);
-} catch (PDOException $e) {
-    echo "ｴﾗｰﾒｯｾｰｼﾞ:{$e->getMessage()}";
-}
-  
-}      
+if(isset($_POST['maxblood'])){
+
+    try {
+
+        $stt = getDB()->prepare('UPDATE user SET maxblood = :maxblood where id = :id');
+        $stt->bindParam(':maxblood', $_POST['maxblood']);
+        $stt->bindParam(':id', $id);
+        $stt->execute();
+        header('Location: https://animech2.herokuapp.com/);
+
+    } catch (PDOException $e) {
+    
+        echo "ｴﾗｰﾒｯｾｰｼﾞ:{$e->getMessage()}";
+        
+    }
+
+}   
+
 ?>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
