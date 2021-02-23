0e20eb55c66067ef3aa9a7dea1db32afd85a6bfe
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-23 18:32:51 +0900

    Update header.php

--- parts/header.php
+++ parts/header.php
@@ -1,8 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-info ">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-info " style="background-color: #e3f2fd;">
+  
   <a class="navbar-brand text-white" href="#" style="margin-left:20px;">利用者一覧</a>
+  
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+  
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
@@ -10,4 +14,5 @@
       </li>
     </ul>
   </div>
+  
 </nav>
