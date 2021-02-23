91601730e3b7fcf279ed4786b5d762eb6313c158
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-23 21:53:43 +0900

    Update index.php

--- search/index.php
+++ search/index.php
@@ -1,134 +1,130 @@
+
 <?php require_once("../parts/head.php"); ?>
+
 <?php require_once("../parts/header.php"); ?>  
+
 <?php require_once("../common/function.php"); ?>
 
-
-
-
 <?php if(($_POST['blood'] == "up")):
-require_once('../parts/th.php'); 
-$db = getDB();
-$stt = $db->prepare('SELECT * FROM user WHERE maxblood >= :maxblood');
-$stt->bindParam(':maxblood', $_POST['maxblood']);
-$stt->execute();
-    while($row = $stt->fetch(PDO::FETCH_ASSOC)) {
-        
-        $id=e($row['id']);
-        $name=e($row['name']); 
-        $visit=e($row['visit']); 
-        $exits=e($row['exits']); 
-        $maxblood=e($row['maxblood']);
-        $miniblood=e($row['miniblood']);
-        $pulse=e($row['pulse']);
-        $bath=e($row['bath']); 
-        $meal=e($row['meal']); 
-        $notices=e($row['notices']);
+    require_once('../parts/th.php'); 
+        $db = getDB();
+        $stt = $db->prepare('SELECT * FROM user WHERE maxblood >= :maxblood');
+        $stt->bindParam(':maxblood', $_POST['maxblood']);
+        $stt->execute();
+            while($row = $stt->fetch(PDO::FETCH_ASSOC)) {
+
+                $id=e($row['id']);
+                $name=e($row['name']); 
+                $visit=e($row['visit']); 
+                $exits=e($row['exits']); 
+                $maxblood=e($row['maxblood']);
+                $miniblood=e($row['miniblood']);
+                $pulse=e($row['pulse']);
+                $bath=e($row['bath']); 
+                $meal=e($row['meal']); 
+                $notices=e($row['notices']);
 ?>  
+
 <tr>
-    <!--お名前-->
+    
     <td onclick="window.location='input/name.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $name; ?></a>
      </td>
-           
-     <!--来所-->
+          
     <td onclick="window.location='input/visit.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $visit; ?></a>
      </td>
         
-     <!--退所-->
     <td onclick="window.location='input/exit.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $exits; ?></a>
      </td>
         
-     <!--最高・最低血圧/脈拍-->
      <td onclick="window.location='input/blood.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;">
      <?php if(isset($maxblood) || ($miniblood) ||( $pulse)){
-         echo $maxblood."/".$miniblood."/".$pulse;
-     }
+     
+                    echo $maxblood."/".$miniblood."/".$pulse;
+     
+                }
       ?></a>
      </td>
         
-      <!--入浴-->
      <td  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $bath; ?></a>
      </td>
-        
-        
-      <!--食事-->
-    <td   
+    
+     <td   
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $meal; ?></a>
      </td>
         
-      <!--特記事項-->
-    <td onclick="window.location='input/notices.php/<?php echo $id ?>/'"  
+    
+     <td onclick="window.location='input/notices.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $notices ?></a>
      </td>
         
 </tr>
 
 <?php } ?>
+
 </table>
+
 <?php require('../parts/numpeople.php'); ?>
+
 <?php endif; ?>
 
-
 <?php if(($_POST['blood'] == "down")):
-require_once('../parts/th.php'); 
-$db = getDB();
-$stt = $db->prepare('SELECT * FROM user WHERE maxblood <= :maxblood');
-$stt->bindParam(':maxblood', e($_POST['maxblood']));
-$stt->execute();
-    while($row = $stt->fetch(PDO::FETCH_ASSOC)) {
-        
-        $id=e($row['id']);
-        $name=e($row['name']); 
-        $visit=e($row['visit']); 
-        $exits=e($row['exits']); 
-        $maxblood=e($row['maxblood']);
-        $miniblood=e($row['miniblood']);
-        $pulse=e($row['pulse']);
-        $bath=e($row['bath']); 
-        $meal=e($row['meal']); 
-        $notices=e($row['notices']);
+
+        require_once('../parts/th.php'); 
+        $db = getDB();
+        $stt = $db->prepare('SELECT * FROM user WHERE maxblood <= :maxblood');
+        $stt->bindParam(':maxblood', e($_POST['maxblood']));
+        $stt->execute();
+            while($row = $stt->fetch(PDO::FETCH_ASSOC)) {
+
+                $id=e($row['id']);
+                $name=e($row['name']); 
+                $visit=e($row['visit']); 
+                $exits=e($row['exits']); 
+                $maxblood=e($row['maxblood']);
+                $miniblood=e($row['miniblood']);
+                $pulse=e($row['pulse']);
+                $bath=e($row['bath']); 
+                $meal=e($row['meal']); 
+                $notices=e($row['notices']);
+
 ?>  
+
 <tr>
-    <!--お名前-->
+    
     <td onclick="window.location='input/name.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $name; ?></a>
      </td>
-           
-     <!--来所-->
+          
     <td onclick="window.location='input/visit.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $visit; ?></a>
      </td>
-        
-     <!--退所-->
+       
     <td onclick="window.location='input/exit.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $exits; ?></a>
      </td>
         
-     <!--最高・最低血圧/脈拍-->
-     <td onclick="window.location='input/blood.php/<?php echo $id ?>/'"  
+    <td onclick="window.location='input/blood.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;">
      <?php if(isset($maxblood) || ($miniblood) ||( $pulse)){
-         echo $maxblood."/".$miniblood."/".$pulse;
+     
+                    echo $maxblood."/".$miniblood."/".$pulse;
      }
       ?></a>
      </td>
         
-      <!--入浴-->
      <td  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $bath; ?></a>
      </td>
-        
-        
-      <!--食事-->
+       
     <td   
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $meal; ?></a>
      </td>
         
-      <!--特記事項-->
     <td onclick="window.location='input/notices.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $notices ?></a>
      </td>
@@ -136,6 +132,9 @@ $stt->execute();
 </tr>
 
 <?php } ?>
+
 </table>
+
 <?php require('../parts/numpeople.php'); ?>
+
 <?php endif; ?>
