eba138979edeba2bc08508ad1abd1ca5bf0cded5
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-23 18:40:33 +0900

    Update head.php

--- parts/head.php
+++ parts/head.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
-<html lang="ja">
 
-<head>
-<script type="text/javascript"></script>
-<meta charset="UTF-8">
-<title>名簿、一覧</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-<link rel="stylesheet" href="../public/styles.css">
-</head>
+<!DOCTYPE html>
+  <html lang="ja">
+  <head>
+    <script type="text/javascript"></script>
+    <meta charset="UTF-8">
+    <title>利用者一覧</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../public/styles.css">
+  </head>
