4ce82b766b88ce06741193b508d0bf7046d0b634
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-23 18:55:52 +0900

    Update function.php

--- common/function.php
+++ common/function.php
@@ -1,43 +1,52 @@
 <?php
-function p($str) {
-	echo $str;	
-}
 
-function e(string $str, string $charset = 'UTF-8'): string {
-    return htmlspecialchars($str, ENT_QUOTES | ENT_HTML5, $charset);
-}
-
-function getDB() {
-
-	try {
-
+	function p($str) {
 		
-	    $pdo = new PDO(
-		    'mysql:dbname=heroku_dc6bcee06683c20;host=us-cdbr-east-02.cleardb.com;charset=utf8mb4',
-	   	    'be79249baf2bce',
-		    '032b4eaf',
-	            [
-			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		    ]
-	);
-	} catch (PDOException $e) {
-		ob_start();
-		header('Content-Type: text/plain; charset=UTF-8', true, 500);
-		exit($e->getMessage());
+		echo $str;	
 		
-    } ob_start();
-      header('Content-Type: text/html; charset=utf-8');
-	  return $pdo;
-}
+	}
+
+	function e(string $str, string $charset = 'UTF-8'): string {
+		
+	    return htmlspecialchars($str, ENT_QUOTES | ENT_HTML5, $charset);
+	
+	}
+
+	function getDB() {
+
+		try {
 
 
-    function getURL() {
-        return $_SERVER["REQUEST_URI"];
-    }
+		    $pdo = new PDO(
+			    'mysql:dbname=heroku_dc6bcee06683c20;host=us-cdbr-east-02.cleardb.com;charset=utf8mb4',
+			    'be79249baf2bce',
+			    '032b4eaf',
+			    [
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			    ]
+		);
+		} catch (PDOException $e) {
+			
+			ob_start();
+			header('Content-Type: text/plain; charset=UTF-8', true, 500);
+			exit($e->getMessage());
 
-    function getInfomation() {
-        $urlarray = explode('/', getURL());
-        $ID = $urlarray[3];
-        return $ID;
-    }
+	    } ob_start();
+	      header('Content-Type: text/html; charset=utf-8');
+		  return $pdo;
+	}
+
+	function getURL() {
+		
+		return $_SERVER["REQUEST_URI"];
+	    
+	}
+
+	function getInfomation() {
+		
+		$urlarray = explode('/', getURL());
+		$ID = $urlarray[3];
+		return $ID;
+		
+	 }
